a6d71921dc1c78202d526401648c1b92440b7eaf
bilalliberty <bilal_liberty@live.com>
2013-08-11 18:50:04 +0300

    Update header

--- arch/arm/mach-msm/board-doubleshot.h
+++ arch/arm/mach-msm/board-doubleshot.h
@@ -101,7 +101,7 @@
 #endif
 #endif
 
-#ifdef CONFIG_FB_MSM_OVERLAY_WRITEBACK
+#ifdef CONFIG_FB_MSM_OVERLAY0_WRITEBACK
 /* width x height x 3 bpp x 2 frame buffer */
 #define MSM_FB_WRITEBACK_SIZE roundup((960 * 540 * 3 * 2), 4096)
 #define MSM_FB_WRITEBACK_OFFSET 0
