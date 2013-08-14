15a71624d4de0801ed905ed8d3c068ed1d87df04
Steve Verhagen <scverhagen@gmail.com>
2013-08-14 11:43:31 -0500

    board-doubleshot.c: fix "missing braces around initializer" warning

--- arch/arm/mach-msm/board-doubleshot.c
+++ arch/arm/mach-msm/board-doubleshot.c
@@ -2804,7 +2804,7 @@ static struct tsens_platform_data pyr_tsens_pdata  = {
 		.tsens_factor		= 1000,
 		.hw_type		= MSM_8660,
 		.tsens_num_sensor	= 6,
-		.slope 			= 702,
+		.slope 			= {702},
 };
 
 /*
